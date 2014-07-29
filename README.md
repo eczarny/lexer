6baebba2e253e1ec7eee3d058fee8ef6b92d6577
Eric Czarny <eczarny@gmail.com>
2014-07-29 19:18:04 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# lexer
+
+[![Build Status](https://travis-ci.org/eczarny/lexer.svg?branch=master)](https://travis-ci.org/eczarny/lexer)
+[![GoDoc](https://godoc.org/github.com/eczarny/lexer?status.svg)](https://godoc.org/github.com/eczarny/lexer)
+
+A Go package that makes building lexical analyzers easy.
+
+Refer to [GoDoc][1] for additional information.
+
+[1]: https://godoc.org/github.com/eczarny/lexer
---
+++ lexer.go
@@ -0,0 +1,193 @@
+// Package lexer makes it easy to build lexical analyzers.
+//
+// Lexical analysis is a common problem in computer science; how can a computer program
+// interpret a sequence of characters in a meaningful way? The canonical example, a
+// compiler, must be able to transform source code written in a programming language into
+// its target language (i.e. object code).
+//
+// To accomplish this task a compiler will often employ a lexical analyzer to convert a
+// sequence of characters (e.g. source code) into a sequence of meaningful groups of
+// characters, or tokens. For example consider this simple Go expression:
+//
+//     x := y + 2.0
+//
+// Go's lexical analyzer would emit IDENT, DEFINE, IDENT, ADD, and FLOAT tokens. Notice
+// that only the information that is relevant to the syntax of the expression is emitted
+// as a token; later, semantic analysis, phases of the Go compiler are responsible for
+// ensuring that the expression consists of valid Go source code.
+//
+// In 2011 Rob Pike presented a lexical analyzer written in Go at the Sydney Google
+// Technology User Group (see http://youtu.be/HxaD_trXwRE). Pike's talk covered many 
+// topics but really converged on two powerful concepts:
+//
+//     1. Representing state and state changes as functions
+//     2. Leveraging goroutines and channels to emit tokens
+//
+// A lexical analyzer can most often be implemented as a state machine. As a lexical
+// analyzer traverses a sequence of characters it can be in one of any number of valid
+// states. To transition between states the lexical analyzer must examine the next
+// character in its sequence of characters and, based on the state it is currently in,
+// determine which valid state to enter.
+//
+// Functions are a great way to represent states, and transitions between states, in a 
+// state machine. The lexical analyzer that is the subject of Pike's talk is inialized 
+// with a function representing its initial state. This function is then able to inspect 
+// the input and, based on what it encounters and its position in that input, can return 
+// the next state the lexical analyzer should transition to.
+//
+// The process of emitting tokens during lexical analysis can be greatly simplified by 
+// taking advantage of Go's concurrency primitives. Running the state machine that drives 
+// the lexical analyzer in a goroutine allows emitted tokens to be sent to a specific
+// channel; decoupling the two responsibilities almost entirely.
+//
+// This package was created as an exercise in learning the Go programming language.
+package lexer
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+// Token, consisting of a type and value, represents the output of the lexer.
+type Token struct {
+	Type  TokenType
+	Value interface{}
+}
+
+// TokenType represents the type of a given token.
+type TokenType int
+
+// TokenError represents a type of token that contains an error message as its value.
+const TokenError TokenType = -1
+
+// EOF represents the end of the input.
+const EOF = rune(-1)
+
+// RunePosition represents the position of a rune in the input.
+type RunePosition int
+
+// RuneWidth represents the width of a rune.
+type RuneWidth int
+
+// StateFunction is a function that represents the state of the lexer.
+type StateFunction func(*Lexer) StateFunction
+
+// RunePredicate is a function that returns true or false based on the specified rune.
+type RunePredicate func(rune) bool
+
+// Lexer contains the lexer's internal state.
+type Lexer struct {
+	Input            string
+	CurrentPosition  RunePosition
+	CurrentRuneWidth RuneWidth
+	initialState     StateFunction
+	startPosition    RunePosition
+	previousToken    Token
+	tokens           chan Token
+}
+
+// NewLexer creates a lexer from the input and initial state.
+func NewLexer(input string, initialState StateFunction) *Lexer {
+	l := &Lexer{
+		Input:        input,
+		initialState: initialState,
+		tokens:       make(chan Token),
+	}
+	go func() {
+		for state := l.initialState; state != nil; {
+			state = state(l)
+		}
+	}()
+	return l
+}
+
+// NextToken returns the next token emitted by the lexer.
+func (l *Lexer) NextToken() Token {
+	return <-l.tokens
+}
+
+// Next returns the next rune from the input and moves the current position of the
+// lexer ahead. If encountering the end of the input EOF will be returned.
+func (l *Lexer) Next() rune {
+	if int(l.CurrentPosition) >= len(l.Input) {
+		l.CurrentRuneWidth = 0
+		return EOF
+	}
+	r, w := utf8.DecodeRuneInString(l.Input[l.CurrentPosition:])
+	l.CurrentRuneWidth = RuneWidth(w)
+	l.CurrentPosition += RunePosition(l.CurrentRuneWidth)
+	return r
+}
+
+// NextForAll returns runes from the input where the specified predicate does apply.
+func (l *Lexer) NextForAll(predicate RunePredicate) []rune {
+	var r rune
+	rs := make([]rune, len(l.Input))
+	for {
+		r = l.Peek()
+		if !predicate(r) || r == EOF {
+			break
+		}
+		rs = append(rs, r)
+		r = l.Next()
+	}
+	return rs
+}
+
+// Peek returns the next rune from the input without moving the current position of the
+// lexer ahead.
+func (l *Lexer) Peek() rune {
+	r := l.Next()
+	l.Previous()
+	return r
+}
+
+// Previous returns the previous rune from the input and moves the current position of the
+// lexer behind.
+func (l *Lexer) Previous() rune {
+	l.CurrentPosition -= RunePosition(l.CurrentRuneWidth)
+	r, _ := utf8.DecodeRuneInString(l.Input[l.CurrentPosition:])
+	return r
+}
+
+// Ignore skips and returns the next rune from the input.
+func (l *Lexer) Ignore() rune {
+	r := l.Next()
+	l.startPosition = l.CurrentPosition
+	return r
+}
+
+// IgnoreUpTo skips runes from the input where the specified predicate does not apply.
+//
+// The rune last seen by the predicate is returned; however, if the end of the input is
+// encountered before the predicate is satisfied EOF is returned instead.
+func (l *Lexer) IgnoreUpTo(predicate RunePredicate) rune {
+	var r rune
+	for {
+		r = l.Peek()
+		if predicate(r) || r == EOF {
+			break
+		}
+		r = l.Ignore()
+	}
+	return r
+}
+
+// Emit emits a token of the specified type.
+func (l *Lexer) Emit(tokenType TokenType) {
+	token := Token{tokenType, l.Input[l.startPosition:l.CurrentPosition]}
+	l.tokens <- token
+	l.previousToken = token
+	l.startPosition = l.CurrentPosition
+}
+
+// PreviousToken returns the most recently emitted token.
+func (l *Lexer) PreviousToken() Token {
+	return l.previousToken
+}
+
+// Errorf emits an error token with the specified error message as its value.
+func (l *Lexer) Errorf(format string, args ...interface{}) StateFunction {
+	l.tokens <- Token{TokenError, fmt.Sprintf(format, args...)}
+	return nil
+}
